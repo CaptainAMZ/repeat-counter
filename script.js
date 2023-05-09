939483ffa40ed1efa270338e1a70b4317888c818
AMZ <alirezamohammadizavele@gmail.com>
2023-05-09 18:01:22 +0430

    fix characters

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let word = "Hello World";
+let reg = /^[A-Za-z0-9]/
 let string = word.split("");
 let repeat = string.reduce((prevValue, currValue) => {
-  return currValue !== " "
+  return currValue.match(reg)
     ? { ...prevValue, [currValue]: (prevValue[currValue] || 0) + 1 }
     : prevValue;
 }, {});
 
 console.log(repeat);
-
-
